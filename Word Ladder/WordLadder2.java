ab21146bc51e412392f0029b9b048368a7f7e07c
NataliaPuchkova <35714707+NataliaPuchkova@users.noreply.github.com>
2018-02-02 17:03:20 -0600

    Create WordLadder2.java

---
+++ Word Ladder/WordLadder2.java
@@ -0,0 +1,16 @@
+class WordLadder2.java{
+/*
+ LeetCode – Word Ladder II (Java)
+ 
+
+Given two words (start and end), and a dictionary, find all shortest transformation sequence(s) from start to end, such that: 
+1) Only one letter can be changed at a time, 
+2) Each intermediate word must exist in the dictionary.
+
+For example, given: start = "hit", end = "cog", and dict = ["hot","dot","dog","lot","log"], return:
+  [
+    ["hit","hot","dot","dog","cog"],
+    ["hit","hot","lot","log","cog"]
+  ]
+  */
+}
