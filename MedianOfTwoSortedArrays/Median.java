f632b27a9076112f5faa60c80e5798e977de50b9
NataliaPuchkova <35714707+NataliaPuchkova@users.noreply.github.com>
2018-02-02 17:05:12 -0600

    Create Median.java

---
+++ MedianOfTwoSortedArrays/Median.java
@@ -0,0 +1,8 @@
+class Median{
+/* This problem can be converted to the problem of finding kth element, k is (A's length + B' Length)/2.
+
+If any of the two arrays is empty, then the kth element is the non-empty array's kth element. If k == 0, the kth element is the first element of A or B.
+
+For normal cases(all other cases), we need to move the pointer at the pace of half of the array size to get log(n) time. 
+*/
+}
