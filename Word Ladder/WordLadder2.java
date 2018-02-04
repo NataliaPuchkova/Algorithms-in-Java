c777f92eb9384af27006df848f31f7cd5c8adb81
NataliaPuchkova <35714707+NataliaPuchkova@users.noreply.github.com>
2018-02-03 20:13:46 -0600

    Update WordLadder2.java

--- Word Ladder/WordLadder2.java
+++ Word Ladder/WordLadder2.java
@@ -13,4 +13,67 @@ For example, given: start = "hit", end = "cog", and dict = ["hot","dot","dog","l
     ["hit","hot","lot","log","cog"]
   ]
   */
+ 
+  public static ArrayList<ArrayList<String>> getPaths(String start, String end, ArrayList<String> dict){
+                // bounders
+                if (start==null || end==null || start.length()!=end.length()|| dict==null || dict.size()==0) return null;
+                if (start.equals(end)) return null;
+
+                ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+                StringBuilder str = new StringBuilder(start);
+                for(char ch='a'; ch<='z'; ch++){
+                        for(int i = 0; i<start.length(); i++){
+                                StringBuilder builder = new StringBuilder(str);
+                                builder.setCharAt(i,ch);
+                                if (builder.toString().equals(end)) {
+                                        ArrayList<String> list = new ArrayList<String>();
+                                        list.add(start);
+                                        list.add(end);
+                                        result.add(list);
+                                }
+                                if (dict.contains(builder.toString())){
+                                        ArrayList<String> list = new ArrayList<String>();
+                                        list.add(start);
+                                        list.add(builder.toString());
+                                        int k = getPaths( builder.toString(), end, dict,list);
+                                        if (k==100){
+                                                list.add(end);
+                                                result.add(list);
+                                        }
+                                }
+                        }
+                }
+                return result;
+        }
+
+        public static int  getPaths(String start, String end, ArrayList<String> dict, ArrayList<String> list){
+                StringBuilder str = new StringBuilder(start);
+                for(char ch='a'; ch<='z'; ch++){
+                        for(int i = 0; i<start.length(); i++){
+                                StringBuilder builder = new StringBuilder(str);
+                                builder.setCharAt(i,ch);
+                                if (builder.toString().equals(end)) return 100;
+                                if (dict.contains(builder.toString()) && !list.contains(builder.toString())){
+                                        list.add(builder.toString());
+                                        //System.out.println(Arrays.toString(list.toArray()));
+                                        return getPaths( builder.toString(), end, dict,list);
+                                }
+                        }
+                }
+                return 0;
+        }
+
+        public static void main(String...arg){
+                String[] ar = {"hot","dot","dog","lot","log"};
+                ArrayList<String> arr = new ArrayList(Arrays.asList(ar));
+                ArrayList<ArrayList<String>> list = getPaths("hit", "cog", arr);
+                for(ArrayList<String> ll : list){
+                        for(String str: ll){
+                                System.out.print(str+",");
+                        }
+                        System.out.println();
+                }
+
+        }
+
 }
