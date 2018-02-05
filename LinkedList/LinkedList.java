605dadd6f15d7d7d3b9157e39133cfe61e1311cd
NataliaPuchkova <35714707+NataliaPuchkova@users.noreply.github.com>
2018-02-04 20:42:11 -0600

    Update LinkedList.java

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,28 +1,29 @@
 class LinkedList{
-   public static LinkedNode insertInFront(Node head, int value){
+   public static LinkedNode insertInFront(LinkedNode head, int value){
                 LinkedNode node = new LinkedNode(null, value);
                 node.next = head;
                 return node;
         }
-   public static LinkedNode search(LinkedNode head, int value){
+
+        public static LinkedNode search(LinkedNode head, int value){
                 if (head==null) return null;
                 if (head.value==value) return head;
-                LinkedList tmp = head.next;
-                while(tmp!=null){
-                        search(tmp, value);
-                }
+                LinkedNode tmp = head.next;
+                return search(tmp, value);
         }
-    public static int count(LinkedNode node){
+        public static int count(LinkedNode node){
                 if (node==null) return 0;
                 else return count(node.next)+1;
         }
-     public static LinkedNode getNth(LinkedList node, int k){
+
+        public static LinkedNode getNth(LinkedNode node, int k){
                 if (k==0) return node;
                 if (node==null) return null;
                 return getNth(node.next, k-1);
         }
-   public static LinkedNode getNthfromEnd(LinkedList node, int k){
-                LinkedNode tmp = node;
+
+        public static LinkedNode getNthfromEnd(LinkedNode node, int k){
+                LinkedNode tmp1 = node;
                 LinkedNode tmp2 = node;
                 while (k!=0){
                         if (tmp1.next==null) return null;
@@ -35,4 +36,5 @@ class LinkedList{
                 }
                 return tmp2;
         }
+
 }
